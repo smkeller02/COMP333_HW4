673421acc881dfc722c636e07c87d40a95af1da9
Sydney Keller <smkeller@wesleyan.edu>
2023-11-11 18:25:56 -0500

    fixing view * ALL MINJI WORK, shes having issues pulling * (issue #68)

--- view.js
+++ view.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
+import { useRoute } from "@react-navigation/native";
 
 // Displays a single ratings information
-function ViewRating({ ratingData }) {
+function ViewRating() {
+  const [user, setUser] = useState(null); // Initialize user state
   const navigation = useNavigation();
 
   const handleBack = () => {
@@ -12,8 +14,25 @@ function ViewRating({ ratingData }) {
     navigation.goBack();
   };
 
+  const route = useRoute();
+  const ratingData = route.params?.ratingData || {};
+
+  // Retrieve the user's name from AsyncStorage
+  AsyncStorage.getItem('user')
+  .then((value) => {
+      if (value) {
+          setUser(value);
+      }
+  })
+  .catch((error) => {
+      console.error(error);
+  });
+
   return (
     <View style={styles.container}>
+      {/* display username at top of screen */}
+      <Text style={styles.userText}>Username: {user}</Text>
+
       <Text>Title: {ratingData.song}</Text>
       <Text>Artist: {ratingData.artist}</Text>
       <Text>Username: {ratingData.username}</Text>
